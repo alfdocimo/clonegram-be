ea77d21a43a274927298e400a455b378db6869c5
Alfredo <alfredo.narvaez@travelport.com>
2020-08-29 20:32:54 +0200

    feat: enhance unit testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [UsersController],
-  providers: [UsersService],
+  imports: [UsersModule],
 })
 export class AppModule {}
